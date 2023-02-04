5015c8aea07eee76fa47a548e8c72aed2bf4abdb
WillPower999 <williamcordelllegensworm@gmail.com>
2023-02-04 13:20:09 -0600

    Main Menu Set Up (basic)

---
+++ GGJ_2023/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject buttonsToActivate;
+    private bool canActivateButtons;
+    [SerializeField] private GameObject gameSelectButtons;
+    private bool gameSelectButtonsActivated;
+    [SerializeField] private Button play;
+    [SerializeField] private Button startGame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonsToActivate.gameObject.SetActive(false);
+        canActivateButtons = true;
+        gameSelectButtons.gameObject.SetActive(false);
+        gameSelectButtonsActivated = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && canActivateButtons)
+        {
+            buttonsToActivate.gameObject.SetActive(true);
+            canActivateButtons = false;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape) && gameSelectButtonsActivated)
+        {
+            gameSelectButtons.gameObject.SetActive(false);
+            gameSelectButtonsActivated = false;
+            buttonsToActivate.gameObject.SetActive(true);
+            play.gameObject.GetComponent<Button>().Select();
+        }
+
+        if(EventSystem.current.currentSelectedGameObject == null)
+        {
+            FindObjectOfType<Button>().Select();
+        }
+
+
+
+    }
+
+    public void PlaySelect()
+    {
+        buttonsToActivate.gameObject.SetActive(false);
+        gameSelectButtons.gameObject.SetActive(true);
+        gameSelectButtonsActivated = true;
+
+        startGame.gameObject.GetComponent<Button>().Select();
+    }
+
+    public void StartGame(string levelToStart)
+    {
+        if(levelToStart == null)
+        {
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            SceneManager.LoadScene(levelToStart);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
