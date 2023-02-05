f14ce62934dab2c07bf801f5b945f30b4fc309bf
JustinBaldridge <justinbaldridge@gmail.com>
2023-02-04 19:10:37 -0600

    ahhhhhh

--- GGJ_2023/Assets/Scripts/PlayerInput.cs
+++ GGJ_2023/Assets/Scripts/PlayerInput.cs
@@ -18,11 +18,11 @@ public class PlayerInput : MonoBehaviour
         Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         player.SetDirectionalInput(directionalInput);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.W))
         {
             player.OnJumpInputDown();
         }
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.W))
         {
             player.OnJumpInputUp();
         }
